c23a75a414c310f5f0e4f1ad40ef3bdb0ba75055
antonio <antoniovictor200@gmail.com>
2022-05-18 20:17:06 -0300

    atualizaçao

--- trabalho.ino
+++ trabalho.ino
@@ -1,11 +1,27 @@
 #define PIN_RES 14
 #define PIN_VENT 15
+#define PIN_SENSOR 17
+
+int Kp = 0;
+int Ki = 0;
+int Kd = 0;
+
+int P, I, D;
+int PID;
+
 void setup() {
   pinMode(PIN_RES, OUTPUT);
   pinMode(PIN_VENT, OUTPUT);
+  pinMode(PIN_SENSOR, INPUT);
+  ledcAttachPin(PIN_RES, 0);
+  ledcAttachPin(PIN_VENT, 1);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+}
+
+void calc_PID() {
+  
+}
 }
